8742e7156ae979868591c1ce8b60d75133b44d67
ChadhaH <chadha.hamed1@gmail.com>
2024-05-08 21:20:18 +0000

    fix

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python3
 """
-	LIFO Caching.
+LIFO Caching
 """
-from collections import OrderedDict
 
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
     """
-	class LIFOCache that inherits from BaseCaching and is a caching system
+     class LIFOCache that inherits from BaseCaching and is a caching system
     """
+
     def __init__(self):
         """
-		Initializes the cache.
+        Init method
         """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.key_indexes = []
 
     def put(self, key, item):
         """
-		assign to the dictionary self.cache_data the item value for the key key
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
         """
-        if key is None or item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.key_indexes.remove(key)
+                else:
+                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
+                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    print("DISCARD:", item_discarded)
+
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
 
     def get(self, key):
         """
-		return the value in self.cache_data linked to key
+        Must return the value in self.cache_data linked to key.
         """
-        return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
